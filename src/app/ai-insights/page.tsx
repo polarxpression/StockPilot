d4adc5baf4e1565902b8055a69a20268f268cb9a
Rafael Braun <rafaelbraun2105@gmail.com>
2025-09-09 19:07:00 +0000

    Try fixing this error: `Build Error: Export runGenerateRestockAlerts doe

--- src/app/ai-insights/page.tsx
+++
@@ -1,9 +0,0 @@
-import AiClient from "./_components/ai-client";
-
-export default function AiInsightsPage() {
-    return (
-        <div className="flex flex-col gap-8">
-            <AiClient />
-        </div>
-    );
-}
