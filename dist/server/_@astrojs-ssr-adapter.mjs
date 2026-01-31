93d1c0d657c4e1b8b71cfc83f6678374f5af7fc4
polarfreez <rafaelbraun5@hotmail.com>
2026-01-31 15:57:18 -0300

    Configure Vercel adapter and update gitignore

--- dist/server/_@astrojs-ssr-adapter.mjs
+++
@@ -1,1 +0,0 @@
-export { c as createExports, a as start } from './chunks/_@astrojs-ssr-adapter_sSoDU0N6.mjs';
--- dist/server/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/node_XYmaL7dF.mjs';
-export { renderers } from '../renderers.mjs';
